c887eef5b253034276e04aa39aa63c50c0891fb8
abas afrah <info@fitfastfoodz.com>
2026-05-07 16:36:47 +0200

    added context for wishlist and cant feature

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -48,6 +48,8 @@ export default function Home() {
        {BANNERS.map((banner, index)=>(
          <View key={index} className='relative w-full h-48 bg-gray-200 overflow-hidden' style={{width: width -32}}>
          <Image source={{uri: banner.image}} className='w-full h-full' resizeMode='cover'/>
+ 
+         <View className='absolute inset-0 bg-black/40'/>
 
          <View className='absolute bottom-4 left-4 z-10'>
           <Text className='text-white text-2xl font-bold'>{banner.title}</Text>
@@ -57,7 +59,7 @@ export default function Home() {
           </TouchableOpacity>
           
          </View>
-         <View className='absolute inset-0 bg-black/40'/>
+        
          </View>
        ))}
     
