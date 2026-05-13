4d3e10dc21da0f609d6cc0811af83213bc480e22
abas afrah <info@fitfastfoodz.com>
2026-05-14 01:12:20 +0200

    updated server

--- server/server.ts
+++ server/server.ts
@@ -3,11 +3,13 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import { clerkMiddleware } from '@clerk/express'
+import { clerkWebhook } from "./controllers/webhooks.js";
 
 const app = express();
 
 const startServer = async () => {
   try {
+    
     // Connect to MongoDB
     await connectDB();
 
@@ -15,6 +17,8 @@ const startServer = async () => {
     app.use(cors());
     app.use(express.json());
     app.use(clerkMiddleware());
+    app.post("/api/clerk", express.raw({ type: "application/json" }), clerkWebhook
+);
 
     const port = process.env.PORT || 3000;
 
